282b15d59792598e549e6c787b3003fa0911a6a9
spardutti <luisdamian.sp@gmail.com>
2021-10-26 18:45:31 -0300

    dropdown

--- src/Components/IngredientsDropdown.jsx
+++ src/Components/IngredientsDropdown.jsx
@@ -1,21 +1,43 @@
-import { useState, useEffect } from "react";
-
-
-
 export const IngredientsDropdown = (props) => {
-  const { ingredients, valueKey, onChangeKey } = props;
-
-  
+  const {
+    arr,
+    ingredientValue,
+    ingredientHandler,
+    quantity,
+    quantityHandler,
+    unit,
+    unitHandler,
+  } = props;
 
   return (
-    <select onChange={onChangeKey} value={ valueKey }>
-      {ingredients.map((ingredient, index) => {
-        return (
-          <option value= "" key={index}>
-            {ingredient.ingredient.name}
-          </option>
-        );
-      })}
-    </select>
+    <div>
+      <select onChange={ingredientHandler} value={ingredientValue}>
+        <option defaultValue="">Escoge un ingrediente</option>
+        {arr.map((ingredient, index) => {
+          return (
+            <option value="" key={index}>
+              {ingredient.name}
+            </option>
+          );
+        })}
+      </select>
+      <div className="input-container">
+        <input
+          type="text"
+          required
+          autoComplete="off"
+          onChange={quantityHandler}
+          value={quantity}
+        />
+        <label htmlFor="">Cantidad</label>
+      </div>
+      <div className="input-container">
+        <select name="" id="" onChange={unitHandler} value={unit}>
+          <option value=""></option>
+          <option value="Grs">Grs.</option>
+          <option value="Ml">Ml.</option>
+        </select>
+      </div>
+    </div>
   );
 };
